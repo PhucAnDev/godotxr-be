4528ea48adb8f092fd527a8b1e6be98dd8a2b15e
PHUCAN\phuca <phucan0147@gmail.com>
2026-08-11 12:15:22 +0700

    update time in to db

--- GodotXR.Domain/Entities/BaseEntity.cs
+++ GodotXR.Domain/Entities/BaseEntity.cs
@@ -1,15 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GodotXR.Domain.Entities
 {
     public abstract class BaseEntity
     {
+        // Giờ Việt Nam = UTC+7 (không phụ thuộc timezone database của OS)
+        private static DateTime VietnamNow => DateTime.UtcNow.AddHours(7);
+
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime CreatedAt { get; set; } = VietnamNow;
         public DateTime? UpdatedAt { get; set; }
         public bool IsDeleted { get; set; } = false;
 
